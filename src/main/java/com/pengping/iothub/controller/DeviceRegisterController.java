f64adf22fc7b4207985bc0b249e10a5802fb34a9
heiqiaokeli1947 <845545659@qq.com>
2021-08-08 15:15:06 +0800

    增加注册接口

--- src/main/java/com/pengping/iothub/controller/DeviceRegisterController.java
+++ src/main/java/com/pengping/iothub/controller/DeviceRegisterController.java
@@ -5,22 +5,21 @@ import com.pengping.iothub.entity.DeviceInfo;
 import com.pengping.iothub.service.DeviceRegisterService;
 import com.pengping.iothub.util.CommonResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/RegisterService")
+@RequestMapping(value = "/RegisterService", method = RequestMethod.POST)
 public class DeviceRegisterController {
     @Autowired
     private DeviceRegisterService deviceRegisterService;
 
+    @ResponseBody
     @RequestMapping("getDeviceInfo")
-    public CommonResponse<?> getDeviceInfo(@PathVariable String uuid){
+    public CommonResponse<?> getDeviceInfo(@RequestParam String uuid){
         System.out.printf("getDeviceInfo:"+uuid);
         DeviceInfo deviceInfo = deviceRegisterService.queryDeviceInfoByUUID(uuid);
         System.out.printf("deviceInfo:"+deviceInfo);
-       return new CommonResponse<>("-1", "balance id not found", deviceInfo);
+       return new CommonResponse<>("0", "ok", deviceInfo);
     }
 
 
